695aecbfa4f8fa9ce2b0987c00ce66214d63ba27
auleki <iamtundegiwa@gmail>
2020-10-15 17:00:05 +0100

    first C# app

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace firstCApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Simple Maths";
+            Console.WriteLine("Welcome to the Latte Shop! Pay you for coffee");
+
+            double cash = Convert.ToDouble(Console.ReadLine());
+
+            if (cash < 5) 
+            {
+                double balance = 5 - cash;
+                Console.WriteLine("You need $" + balance + " more");
+            }
+             else if (cash == 5)
+            {
+               
+                Console.WriteLine("Enjoy your coffee");
+            }
+            else
+            {
+                double change = cash - 5;
+                Console.WriteLine("Your change is $" + change);
+            }
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
+
